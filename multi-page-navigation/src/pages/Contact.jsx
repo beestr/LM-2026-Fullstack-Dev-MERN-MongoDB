4cd9d20a4d1f39a7bdb8a1fa121426fdf2d76c83
beestr <belva.calista04@gmail.com>
2026-05-05 11:25:48 +0700

    multi-page-navigation

---
+++ multi-page-navigation/src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+function Contact() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <button onClick={() => navigate("/")}>
+        Go to Home
+      </button>
+    </div>
+  );
+}
+
+export default Contact;
